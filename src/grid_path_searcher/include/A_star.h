54bfd803aaf8bd967e064a5b03e945f6ab026903
YifeiNie <2096935953@qq.com>
2024-12-27 03:51:30 +0800

    A* expand neighbors

--- src/grid_path_searcher/include/A_star.h
+++ src/grid_path_searcher/include/A_star.h
@@ -32,7 +32,11 @@ class AStarManager
 private: 
     
     GridNode ****GridNodeMap;
-    uint8_t * obstacle_map;     // 一个三维映射到一维的数组，存储的0或1表示当前索引的栅格是否有障碍物
+    uint8_t *obstacle_map;     // 一个三维映射到一维的数组，存储的0或1表示当前索引的栅格是否有障碍物\
+
+    std::vector<GridNode*> neighbor_ptr_set;
+    std::vector<double> edge_cost_set;
+
     float rezolution;
     float max_x_coord, max_y_coord, max_z_coord;   // 坐标值，
     float min_x_coord, min_y_coord, min_z_coord;
@@ -40,7 +44,7 @@ private:
     // 由于访问数组的索引是从0开始，所以后面进行遍历时候是"< max_x_idx"而不是"<= max_x_idx"
     int max_x_idx, max_y_idx, max_z_idx;                //索引值，(min_x_coord, min_x_coord, min_z_coord)被认为是(0, 0, 0)索引
     int min_x_idx, min_y_idx, min_z_idx = 0;
-    float max_yz_idx = max_y_idx * max_z_idx;           // 用于三维数组的一维映射时，减少重复计算
+    int max_yz_idx = max_y_idx * max_z_idx;           // 用于三维数组的一维映射时，减少重复计算
     int grid_num = max_x_idx * max_x_idx * max_z_idx;   // 整个地图总共的栅格数量，也即映射到一维时的数组大小
 
     Eigen::Vector3i goal_idx;   // 目标点的索引
@@ -50,16 +54,16 @@ private:
 	std::vector<GridNode*> grid_path;
 	std::vector<GridNode*> endPtrList;
 
-    bool is_obstacle(Eigen::vector3i idx);
+    bool is_obstacle(Eigen::Vector3i idx);
 
 public:
     void gridmap_init(double resolution, Eigen::Vector3d max_coord, Eigen::Vector3d min_coord, int max_x_idx, int max_y_idx, int max_z_idx);
-    inline Eigen::Vector31 coord2idx(int x, int y, int z);
+    inline Eigen::Vector3i coord2idx(int x, int y, int z);
     inline Eigen::Vector3d idx2coord(Eigen::Vector3i tem_idx);
-    void set_obstacle(GridNode* GridNodePtr);
-    void A_star_expand_neighbors(Eigen::vector3i idx);
-    void A_star_search(Eigen::vector3d start_coord, Eigen::vector3d goal_coord);
+    void set_obstacle(Eigen::Vector3i idx);
+    void A_star_expand_neighbors(GridNode* GridNodePtr);
+    void A_star_search(Eigen::Vector3d start_coord, Eigen::Vector3d goal_coord);
     
-}
+};
 
 #endif
--- src/grid_path_searcher/src/A_star.cpp
+++ src/grid_path_searcher/src/A_star.cpp
@@ -24,6 +24,8 @@ GridNode::GridNode(Eigen::Vector3i idx, Eigen::Vector3d coord)
 void AStarManager::gridmap_init(double _resolution, Eigen::Vector3d max_coord, Eigen::Vector3d min_coord, int max_x_idx, int max_y_idx, int max_z_idx)
 {
     this->resolution = resolution;
+    neighbor_ptr_set.clear();
+    edge_cost_set.clear();
 
     max_x_coord = max_coord(0);
     max_y_coord = max_coord(1);
@@ -38,10 +40,11 @@ void AStarManager::gridmap_init(double _resolution, Eigen::Vector3d max_coord, E
     this->max_y_idx = max_y_idx;
     this->max_z_idx = max_z_idx;
 
+    // 计算栅格总数
     grid_num = max_x_idx * max_y_idx * max_z_idx;
 
-    data = new uint8_t[grid_num];
-    memset(data, 0, grid_num * sizeof(uint8_t));
+    obstacle_map = new uint8_t[grid_num];
+    memset(obstacle_map, 0, grid_num * sizeof(uint8_t));
 
     GridNodeMap = new GridNode ***[max_x_idx];
     for (int i = 0; i < max_x_idx; i++)
@@ -52,28 +55,53 @@ void AStarManager::gridmap_init(double _resolution, Eigen::Vector3d max_coord, E
             GridNodeMap[i][j] = new GridNode *[max_z_idx];
             for (int k = 0; k < max_z_idx; k++)
             {
-                Eigen::Vector3i tem_idx(i, j, k);
-                Eigen::Vector3d tem_coord = idx2coord(tem_idx);
-                GridNodeMap[i][j][k] = new GridNode(tem_idx ,tem_coord);
+                Eigen::Vector3i temp_idx(i, j, k);
+                Eigen::Vector3d temp_coord = idx2coord(temp_idx);
+                GridNodeMap[i][j][k] = new GridNode(temp_idx ,temp_coord);
             }
         }
     }
 }
 
 // 返回某个栅格是否为障碍物栅格
-bool AStarManager::is_obstacle(Eigen::vector3i idx)
+bool AStarManager::is_obstacle(Eigen::Vector3i idx)
 {
-    uint8_t = idx(0)>=0 && idx(0)<max_x_idx && idx(1)>=0 && idx(1)<max_y_idx && idx(2)>=0 && idx(2)<max_z_idx;
-    return uint8_t && data[idx(0)*max_yz_idx + idx(1)* max_z_idx + idx(2)];
+    uint8_t temp_is_out_of_range = idx(0)>=0 && idx(0)<max_x_idx && idx(1)>=0 && idx(1)<max_y_idx && idx(2)>=0 && idx(2)<max_z_idx;
+    return temp_is_out_of_range && obstacle_map[idx(0)*max_yz_idx + idx(1)* max_z_idx + idx(2)];
 }
 
-void AStarManager::set_obstacle(Eigen::vector3i idx)
+void AStarManager::set_obstacle(Eigen::Vector3i idx)
 {
-    data[idx(0)*max_yz_idx + idx(1)* max_z_idx + idx(2)] = 1
+    obstacle_map[idx(0)*max_yz_idx + idx(1)* max_z_idx + idx(2)] = 1;
 }
 
-//
+// 查找当前节点的邻居，进行处理后按规则push进优先级队列
+void AStarManager::A_star_expand_neighbors(GridNode* GridNodePtr)
+{
+    for (int i=-1; i<=1; i++)
+    {
+        for (int j=-1; j<=1; j++)
+        {
+            for (int k=-1; k <= 1; k++)
+            {
+                if (i==0 && j==0 && k==0)
+                    continue;
+                Eigen::Vector3i temp_idx = GridNodePtr->idx + Eigen::Vector3i(i, j, k);
+                GridNode* temp_ptr = GridNodeMap[temp_idx(0)][temp_idx(1)][temp_idx(2)];
+               
+                if (is_obstacle(GridNodePtr->idx) || temp_idx(0)<0 || temp_idx(0)>=max_x_idx \
+                                                  || temp_idx(1)<0 || temp_idx(1)>=max_y_idx \
+                                                  || temp_idx(2)<0 || temp_idx(2)>=max_z_idx \
+                                                  || temp_ptr->is_close)
+                    continue;    // 当检查到的邻居栅格：1.是障碍物 2.超出界限 3.在close_list时，跳过
+                neighbor_ptr_set.emplace_back(temp_ptr);
+                edge_cost_set.emplace_back((GridNodePtr->coord - temp_ptr->coord).norm());
+                
+            }
+        }
+    }
 
+}
 
 
 
